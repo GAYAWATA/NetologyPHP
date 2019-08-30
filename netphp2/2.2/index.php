473a14f7791513e117515b36c039b2a9b24725e7
GAYAWATA <44050985+GAYAWATA@users.noreply.github.com>
2019-08-30 15:24:43 +0300

    Update index.php

--- netphp2/2.2/index.php
+++ netphp2/2.2/index.php
@@ -1,6 +1,12 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>	
 <?php
 if (isset($_POST['go'])) {
-    echo 'Форма отправлена<br>';   
+    echo 'Данные получены<br>';   
 if (isset ($_POST['firstName'])) $FirstName = $_POST['firstName'];
 else $FirstName = '';
 if (isset ($_POST['lastName'])) $LastName = $_POST['lastName'];
@@ -10,13 +16,13 @@ else $MiddleName = '';
 if (isset ($_POST['code'])) $CodeWord2 = $_POST['code'];
 }
 
- $fullName = ucfirst(mb_substr($FirstName, 0, 1)) . mb_substr($FirstName, 1,3) . " " . ucfirst(mb_substr($LastName, 0, 1)) . (mb_substr($LastName, 1, 5)) . " " . ucfirst(mb_substr($MiddleName, 0,1)) . (mb_substr($MiddleName, 1,7));
- $fio = substr_replace($FirstName,' ',1) . substr_replace($LastName,' ',1) . substr_replace($MiddleName,' ',1);
- $surnameAndInitials = mb_substr( $LastName, 0,1) . mb_substr( $LastName, 1,5) . " " . ucfirst(substr_replace($FirstName,' ',1)) . " " . ucfirst(substr_replace($MiddleName,' ',1));
+$fullName = mb_strtoupper(mb_substr($FirstName, 0,1)) . mb_substr($FirstName, 1) . " " . mb_strtoupper(mb_substr($LastName, 0, 1)) . mb_substr($LastName, 1) . " " . mb_strtoupper(mb_substr($MiddleName, 0,1)) . (mb_substr($MiddleName, 1,7));
+$fio = mb_strtoupper(mb_substr($FirstName, 0,1)) . "." . mb_strtoupper(mb_substr($LastName, 0, 1)) . "." . mb_strtoupper(mb_substr($MiddleName, 0,1)) . ".";
+$surnameAndInitials = mb_strtoupper(mb_substr($LastName, 0, 1)) . mb_substr($LastName, 1) . " " . mb_strtoupper(mb_substr($FirstName, 0,1)) . "." . mb_strtoupper(mb_substr($MiddleName, 0,1)) . ".";
  ?>
-
- <html>
+   <body>
      <p> Полное имя: <?php echo $fullName ?></p>
-     <p>Аббревиатура: ' <?php echo strtoupper($fio)?> '</p>
+     <p>Аббревиатура: ' <?php echo $fio ?> '</p>
      <p>Фамилия и инициалы: <?php echo ucfirst($surnameAndInitials) ?></p>
+   </body>    
  </html>
